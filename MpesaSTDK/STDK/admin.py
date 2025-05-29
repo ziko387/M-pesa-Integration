a94ee29fed73d4a24b2b23aeba22be33a68b9e86
ziko387 <mwangiian973@gmail.com>
2025-05-29 15:32:01 +0300

    added a django file

--- MpesaSTDK/STDK/admin.py
+++ MpesaSTDK/STDK/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from.models import Transaction
 # Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('transaction_id', 'phone_number', 'amount', 'mpesa_receipt_number',
+                    'date_created','transaction_date')
+    list_filter = ('status','date_created','transaction_date')
+    search_fields = ('transaction_id','phone_number','mpesa_receipt_number')
